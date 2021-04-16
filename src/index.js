6a5da652f0829e99ae2775ffc47525ed198a6063
Sridhar Rajagopalan <sridhar@stepzen.com>
2021-04-16 15:56:05 +0100

    Upload and deploy.

--- src/index.js
+++ src/index.js
@@ -11,15 +11,25 @@ module.exports = {
     console.log('Build')
     const stepzenSecret = args.netlifyConfig.build.environment.STEPZEN_ADMIN_KEY
     const stepzenAccount = args.netlifyConfig.build.environment.STEPZEN_ACCOUNT
-    const stepzenSchema = args.netlifyConfig.build.environment.STEPZEN_SCHEMA_NAME
-    const stepzenEndpoint = args.netlifyConfig.build.environment.STEPZEN_ENDPOINT
-    const stepzenConfiguration = args.netlifyConfig.build.environment.STEPZEN_CONFIGURATIONSETS
+    const stepzenSchema = args.netlifyConfig.build.environment.STEPZEN_SCHEMA_NAME || 'schema'
+    const stepzenEndpoint = args.netlifyConfig.build.environment.STEPZEN_ENDPOINT || 'endpoint'
+    const stepzenConfiguration = args.netlifyConfig.build.environment.STEPZEN_CONFIGURATIONSETS || 'configuration'
+    const stepzenFolder = args.netlifyConfig.build.environment.STEPZEN_FOLDER || 'netlify'
 
-    console.log(`pushing schema to ${stepzenSchema}, and deploying to ${stepzenEndpoint} using ${stepzenConfiguration}`)
+    console.log(chalk.white(`using ${stepzenAccount}`))
+    console.log(chalk.white(`pushing schema to ${stepzenFolder}/${stepzenSchema}, and deploying to ${stepzenFolder}/${stepzenEndpoint} using ${stepzenFolder}/${stepzenConfiguration}`))
     const client = await stepzen.client({
       account: stepzenAccount,
       adminkey: stepzenSecret,
     })
+    await client.upload.schema(`${stepzenFolder}/${stepzenSchema}`, "stepzen")
+    await client.deploy(
+      `${stepzenFolder}/${stepzenEndpoint}`,
+      {
+        configurationsets: [`${stepzenFolder}/${stepzenConfiguration}`],
+        schema: `${stepzenFolder}/${stepzenSchema}`,
+      },
+    )
     args.utils.status.show({summary: 'Success!'})
   },
   async onPostBuild( args ) {},
