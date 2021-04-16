9a52bd999ba77573d4ea9fc3e0ff7ce95448991b
Sridhar Rajagopalan <sridhar@stepzen.com>
2021-04-16 14:52:45 +0100

    Remove reference to lodash

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // This is the main file for the Netlify Build plugin stepzen.
-const lodash = require('lodash')
+const chalk = require('chalk')
 const stepzen = require('@stepzen/sdk')
 
 module.exports = {
@@ -14,6 +14,7 @@ module.exports = {
     const stepzenSchema = args.netlifyConfig.build.environment.STEPZEN_SCHEMA_NAME
     const stepzenEndpoint = args.netlifyConfig.build.environment.STEPZEN_ENDPOINT
     const stepzenConfiguration = args.netlifyConfig.build.environment.STEPZEN_CONFIGURATIONSETS
+
     console.log(`pushing schema to ${stepzenSchema}, and deploying to ${stepzenEndpoint} using ${stepzenConfiguration}`)
     const client = await stepzen.client({
       account: stepzenAccount,
